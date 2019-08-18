6e7f7f0bba15412144532a9eadbbb0e090c3608c
sayanta <sayantabasu@outlook.com>
2019-08-19 00:05:27 +0530

    Initial push

---
+++ AlgoCasts/exercises/reversestring/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    //return str.split('').reverse().join('');
+    let reversed = '';
+    for(let char of str){
+        reversed = char + reversed;
+    }
+    return reversed;
+}
+
+module.exports = reverse;
